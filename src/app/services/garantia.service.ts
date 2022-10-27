8fe9e578095f85464d170d4b5a693bbd236cf0a5
Ricardo Hernandez Limon <richi8297@gmail.com>
2022-10-27 00:35:48 -0500

    Sirve proxy

--- src/app/services/garantia.service.ts
+++ src/app/services/garantia.service.ts
@@ -10,28 +10,28 @@ export class GarantiaService {
 
   //Api
   //API_URI = 'http://10.0.1.49:3000/api'
-  API_URI = 'http://10.0.1.49:3000/';
+  API_URI = 'http://localhost:3000/api';
   constructor(private http: HttpClient) { }
 
 
   getGarantias(){
-  return this.http.get(`${this.API_URI}/garantia`);
+  return this.http.get(`/api/garantia`);
    }
 
    getGarantia(id: string){
-   return this.http.get(`${this.API_URI}/garantia/${id}`);
+   return this.http.get(`/api/garantia/${id}`);
    }
 
    deleteGarantia(id: string){
-   return this.http.delete(`${this.API_URI}/garantia/${id}`);
+   return this.http.delete(`/api/garantia/${id}`);
    }
 
    saveGarantia(game: garantia){
-   return this.http.post(`${this.API_URI}/garantia`,game);
+   return this.http.post(`/api/garantia`,game);
    }
 
    updateGarantia(id: string|number,updatedGame: garantia){
-   return this.http.put(`${this.API_URI}/garantia/${id}`, updatedGame);
+   return this.http.put(`/api/garantia/${id}`, updatedGame);
    }
 
 }
--- src/app/services/reabastecimiento.service.ts
+++ src/app/services/reabastecimiento.service.ts
@@ -11,45 +11,45 @@ export class ReabastecimientoService {
 
 //Api
 //API_URI = 'http://10.0.1.49:3000/api'
-API_URI = 'http://10.0.1.49:3000/';
+API_URI = 'http://localhost:3000/api';
 constructor(private http: HttpClient, private authService: AuthService) { }
 
 
 getReportesCompras(){
   const headers = new HttpHeaders()
   .set('Authorization', 'Bearer ' + this.authService.getToken());
-  return this.http.get(`${this.API_URI}/reabastecimiento`, { headers });
+  return this.http.get(`/api/reabastecimiento`, { headers });
  }
 
  getReporteCompras(id: string){
   const headers = new HttpHeaders()
   .set('Authorization', 'Bearer ' + this.authService.getToken());
-  return this.http.get(`${this.API_URI}/reabastecimiento/${id}`, { headers });
+  return this.http.get(`/api/reabastecimiento/${id}`, { headers });
  }
 
  deleteReporteCompras(id: string){
   const headers = new HttpHeaders()
   .set('Authorization', 'Bearer ' + this.authService.getToken());
-  return this.http.delete(`${this.API_URI}/reabastecimiento/${id}`, { headers });
+  return this.http.delete(`/api/reabastecimiento/${id}`, { headers });
  }
 
  //Necesito un juego de tipo juego
  saveReporteReabastecimiento(updateReabastecimiento: Reabastecimiento){
   const headers = new HttpHeaders()
   .set('Authorization', 'Bearer ' + this.authService.getToken());
-  return this.http.post(`${this.API_URI}/reabastecimiento`,updateReabastecimiento, { headers });
+  return this.http.post(`/api/reabastecimiento`,updateReabastecimiento, { headers });
  }
 
  updateReporteCompras(id: string|number,updateReabastecimiento: Reabastecimiento){
   const headers = new HttpHeaders()
   .set('Authorization', 'Bearer ' + this.authService.getToken());
-  return this.http.put(`${this.API_URI}/reabastecimiento/${id}`, updateReabastecimiento, { headers });
+  return this.http.put(`/api/reabastecimiento/${id}`, updateReabastecimiento, { headers });
  }
 
  getporFechas(fecha : fechas){
   const headers = new HttpHeaders()
     .set('Authorization', 'Bearer ' + this.authService.getToken());
-  return this.http.post(`${this.API_URI}/reabastecimiento/buscar/uno`,fecha,{ headers });
+  return this.http.post(`/api/reabastecimiento/buscar/uno`,fecha,{ headers });
 }
 
 }
